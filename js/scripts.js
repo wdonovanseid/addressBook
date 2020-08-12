4c1e1a726b55c09cf9daaa080af2eb1ded2c87db
Alma <a.q.frankenstein@gmail.com>
2020-08-12 15:55:28 -0700

    add function to prepend new contacts to list
    co-authored by: William Donovan-Seid <wdstwo@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    const fullname = $("#name").val();
+    const fulladdress = $("#address").val();
+    const fullphone = $("#phone").val();
+    $("ul#contacts").prepend("<li class='clickable'>Why hello there!</li>");
+  });
+  
   $(".clickable").click(function() {
-    $("#norway-showing").toggle();
-    $("#norway-hidden").toggle();
+    $("#contact-showing").toggle();
+    $("#contact-hidden").toggle();
   });
 });
